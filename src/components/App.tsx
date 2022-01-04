c075273ec1829cdb9130a377483dbdc5cbf38f27
Pavel <gdezpd@gmail.com>
2022-01-04 22:38:45 +0300

    add two string

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import {Tehnologies} from './Tehnologies'
+import {Header} from './Header'
+
+
+export const App = () =>{
+  return (
+    <div>
+      <Header/>
+      <Tehnologies/>
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () =>{
+    return( <div>
+        <a href='#'> Home</a>-
+        <a href='#'> News Feed</a>-
+        <a href='#'> Messages</a>-
+    </div>);
+}
+
